b02d5af83095987ce885c54c50a0ff659ca8bfc6
AliArshadswl <aliarshadciitswl@gmail.com>
2021-03-08 17:27:51 +0800

    Update DaiToken.sol

--- Week 5/DaiToken.sol
+++ Week 5/DaiToken.sol
@@ -30,18 +30,18 @@ contract DaiToken {
         return totalSupply_;
     }
     
-    /// @param address_owner The address from which the balance will be retrieved
-    /// @return The balance
+    // @param address_owner The address from which the balance will be retrieved
+    // @return The balance
     function balanceOf(address _owner) public view returns (uint256) {
         return balances[_owner];
     }
     
     
     
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
+    // @notice send `_value` token to `_to` from `msg.sender`
+    // @param _to The address of the recipient
+    // @param _value The amount of token to be transferred
+    // @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
@@ -50,21 +50,21 @@ contract DaiToken {
         return true;
     }
     
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of wei to be approved for transfer
-    /// @return Whether the approval was successful or not
+    // @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    // @param _spender The address of the account able to transfer the tokens
+    // @param _value The amount of wei to be approved for transfer
+    // @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
     
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
+    // @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    // @param _from The address of the sender
+    // @param _to The address of the recipient
+    // @param _value The amount of token to be transferred
+    // @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balances[_from]);
         require(_value <= allowed[_from][msg.sender]);
@@ -74,9 +74,9 @@ contract DaiToken {
         emit Transfer(_from, _to, _value);
         return true;
     }
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
+    // @param _owner The address of the account owning tokens
+    // @param _spender The address of the account able to transfer the tokens
+    // @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
