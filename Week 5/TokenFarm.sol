83966a2f80cbb062da852f40991d9a9be459391f
AliArshadswl <aliarshadciitswl@gmail.com>
2021-03-08 15:56:34 +0800

    Update TokenFarm.sol

--- Week 5/TokenFarm.sol
+++ Week 5/TokenFarm.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.6.0;
-
+// Import contract for both dapp anf DAI token.
 import "./DaiToken.sol";
 import "./DappToken.sol";
 
@@ -19,12 +19,22 @@ contract TokenFarm{
     constructor (DaiToken _DaiToken, DappToken _DappToken) public{
         dappToken = _DappToken;
         daiToken = _DaiToken;
-        owner = msg.sender;
+        owner = msg.sender; // address of the owner of the contract
     }
-
+    
+    /// @param _amount The amount of the tokens you want to stake.
     function stakeToken(uint _amount) public {
-        require(_amount>0,"amount need to be more than 0");
-        daiToken.transferFrom(msg.sender, address(this), _amount);
+        
+        // check, amount should be greater than zero. There should be some tokens to be staked.
+        require(_amount>0,"amount need to be more than 0");         
+        
+        // this refers to the instance of the contract where the call is made (you can have multiple instances of the same contract).
+        // address(this) refers to the address of the instance of the contract where the call is being made.
+        // msg. sender refers to the address where the contract is being called from.
+        // @param _amount, the amount of tokens you want to stake .
+        daiToken.transferFrom(msg.sender, address(this), _amount); 
+        
+        // The balance of the owner of the contract, after staking the coins.
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
         if(!hasStaked[msg.sender]){
@@ -39,11 +49,12 @@ contract TokenFarm{
         daiToken.transfer(msg.sender,balance);
         isStaking[msg.sender] = false;
     }
-
+   //@param _owner is the address of the owner which is msg.sender
+   // returns the staking balance
     function stakeAmount(address _owner) public view returns(uint) {
         returns stakingBalance[_owner];
     }
-
+//
     function issusToken() public {
         require(msg.sender==owner,"trader is not owner");
         for(uint i=0; i<staker.length;i++){
